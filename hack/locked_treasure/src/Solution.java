67b565e2a5bfa63810023d53a954d18ef7ce7eee
valerii.ryzhuk <valerii.ryzhuk@globallogic.com>
2015-10-17 12:41:34 -0400

    Initial IDEA commit with hack module, locked treasure module and Solution template

---
+++ hack/locked_treasure/src/Solution.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.exit;
+
+public class Solution {
+    static BufferedReader in;
+    static PrintWriter out;
+    static StringTokenizer tok;
+    static int test;
+
+    public static void main(String[] args) {
+        try {
+            in = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(new OutputStreamWriter(System.out));
+            test = 1;
+
+            while(hasNext()) {
+                out.println("Case " + test + ": " + solve());
+
+                ++test;
+            }
+            in.close();
+            out.close();
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+            exit(1);
+        }
+    }
+
+    private static String solve() throws Exception {
+
+        return null;
+    }
+
+    private static int nextInt() throws IOException {
+        return parseInt(next());
+    }
+
+    private static long nextLong() throws IOException {
+        return parseLong(next());
+    }
+
+    private static double nextDouble() throws IOException {
+        return parseDouble(next());
+    }
+
+    private static String next() throws IOException {
+        while (tok == null || !tok.hasMoreTokens()) {
+            tok = new StringTokenizer(in.readLine().trim());
+        }
+        return tok.nextToken();
+    }
+
+    private static boolean hasNext() throws Exception {
+        if (tok == null || !tok.hasMoreTokens()) {
+            String line = in.readLine();
+            if(line != null) {
+                tok = new StringTokenizer(line.trim());
+                return hasNext();
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
